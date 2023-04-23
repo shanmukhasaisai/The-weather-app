f2ca19b202b8d61e0ed27118d3bad15467d295ea
Nimmana Shanmukha Sai <89311670+shanmukhasaisai@users.noreply.github.com>
2023-04-23 17:08:57 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+const { json, query } = require("express");
+const express=require("express");
+const https=require("https");
+const bodyParser=require("body-parser");
+const app=express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/",function(req,res){
+   
+    res.sendFile(__dirname+"/index.html");
+    
+});
+
+app.post("/",function(req,res){
+
+     const query =req.body.cityName;
+    const apiKey="64e72919b850a59c7021ddd4b580274e";
+    const unit="metric";
+
+    var url="https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid="+ apiKey +"&units="+unit+""
+    https.get(url,function(response){
+        console.log(response);
+        response.on("data",function(data){
+            const weatherData= JSON.parse(data);
+            const temp=weatherData.main.temp;
+            const icon=weatherData.weather[0].icon;
+            const imgUrl="http://openweathermap.org/img/wn/"+icon+"@2x.png"
+            console.log(temp);
+            res.write("<h1>the temperature in "+query+" is "+ temp+" degree celsius</h1> ");
+            res.write("<img src="+imgUrl+">");
+            res.send();
+        });
+    });
+
+
+});
+
+
+app.listen(3000,function(){
+    console.log("server is running ");
+})
